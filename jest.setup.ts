034f4dc43a4062f765830b52d7be063dc637cbd9
Rubenhdz20 <rubhernandez42@gmail.com>
2026-03-28 18:28:39 -0600

    fix jest.setup.ts href type error

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,6 +1,13 @@
 import "@testing-library/jest-dom/jest-globals"
 import { jest } from "@jest/globals"
 import React from "react"
+import type { UrlObject } from "url"
+
+
+type LinkProps = {
+  href: string | UrlObject
+  children?: React.ReactNode
+} & React.AnchorHTMLAttributes<HTMLAnchorElement>
 
 jest.mock("next/image", () => ({
   __esModule: true,
@@ -25,14 +32,11 @@ jest.mock("next/link", () => ({
     children,
     href,
     ...props
-  }: React.AnchorHTMLAttributes<HTMLAnchorElement> & {
-    href: string | { pathname?: string }
-    children: React.ReactNode
-  }) =>
+  }: LinkProps) =>
     React.createElement(
       "a",
       {
-        href: typeof href === "string" ? href : href.pathname ?? "",
+        href: typeof href === "string" ? href : (href as UrlObject).pathname ?? "",
         ...props,
       },
       children,
